c4322cafe737ac7bc4b31843a324ae8d81f0dcdb
Sethu <sethuserge@gmail.com>
2024-04-12 12:49:01 +0200

    add employee entity

---
+++ ems-back-end/src/main/java/com/sethu/ems/entity/Employee.java
@@ -0,0 +1,30 @@
+package com.sethu.ems.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    String firstName;
+
+    @Column(name = "last_name")
+    String lastName;
+
+    @Column(name = "email_id", nullable = false, unique = true)
+    String email;
+}
