8b08e1e7fe063623d200ce40c3b4cac5648a60a9
Sethu <sethuserge@gmail.com>
2024-04-12 14:15:02 +0200

    Add EmployeeRepository

---
+++ ems-back-end/src/main/java/com/sethu/ems/repository/EmployeeRepository.java
@@ -0,0 +1,7 @@
+package com.sethu.ems.repository;
+
+import com.sethu.ems.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository  extends JpaRepository<Employee, Long> {
+}
